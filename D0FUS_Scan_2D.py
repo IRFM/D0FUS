bd43b876eeefa0c53e97dcc2346ea8ddbe1a2ef0
AUCLAIR Timothé 276941 <timothe.auclair@cea.fr>
2025-01-09 13:54:14 +0000

    Upload New File

---
+++ D0FUS_Scan_2D.py
@@ -0,0 +1,361 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 23 15:08:01 2024
+
+@author: TA276941
+"""
+#%% Import
+import sys
+import os
+import importlib
+
+# Ajouter le répertoire 'D0FUS_BIB' au chemin de recherche de Python
+sys.path.append(os.path.join(os.path.dirname(__file__), 'D0FUS_BIB'))
+
+if "D0FUS_parameterization" in sys.modules:
+    # Le module est déjà importé, le recharger
+    importlib.reload(sys.modules['D0FUS_parameterization'])
+    # Mettre à jour les variables globales
+    globals().update({k: getattr(sys.modules['D0FUS_parameterization'], k) for k in dir(sys.modules['D0FUS_parameterization']) if not k.startswith('_')})
+    importlib.reload(sys.modules['D0FUS_core'])
+    importlib.reload(sys.modules['D0FUS_plot'])
+    print("-----D0FUS Reloaded-----")
+else:
+    # Le module n'est pas encore importé, l'importer
+    from D0FUS_initialisation import *
+    print("-----D0FUS loaded-----")
+    
+# Attention, lors de toute modification de fichiers exterieurs, il est conseillé, malgré l'implémentation de la fonction reaload, de redémarrer le Kernel.
+# Python est capricieux avec le stockage des variables et seul un redmérage du kernel puis import (de nouveau) de labibliothèque est sur à 100%
+# Le rechargement d'un module (importlib.reload) ne met en effet pas forcément à jour les références existantes dans d'autres modules importés précédemment
+# La fonction reload est cependant , du moins dans les versions python de 3.1 à 3.3 permettre de palier à ce problème
+    
+#%% Variation of 1 chosen parameter and mapping the a space
+# 2D matrix calculation
+
+def R0_a_scan(Bmax,P_fus,κ,δ):
+    
+    a_min = 0.2
+    a_max = 3
+    a_values = np.arange(a_min,a_max,0.05)
+    R0_values = np.arange(2,10,0.1)
+     
+    # Matrix creations
+    max_limits_density = np.zeros((len(a_values),len(R0_values)))
+    max_limits_security = np.zeros((len(a_values),len(R0_values)))
+    max_limits_beta = np.zeros((len(a_values),len(R0_values)))
+    radial_build_matrix = np.zeros((len(a_values),len(R0_values)))
+    max_limits_matrix = np.zeros((len(a_values),len(R0_values)))
+    
+    Heat_matrix = np.zeros((len(a_values),len(R0_values)))
+    Cost_matrix = np.zeros((len(a_values),len(R0_values)))
+    Q_matrix = np.zeros((len(a_values),len(R0_values)))
+    P_CD_matrix = np.zeros((len(a_values),len(R0_values)))
+    Gamma_n_matrix = np.zeros((len(a_values),len(R0_values)))
+    L_H_matrix = np.zeros((len(a_values),len(R0_values)))
+    f_alpha_matrix = np.zeros((len(a_values),len(R0_values)))
+    TF_ratio_matrix = np.zeros((len(a_values),len(R0_values)))
+    
+    Ip_matrix = np.zeros((len(a_values),len(R0_values)))
+    n_matrix = np.zeros((len(a_values),len(R0_values)))
+    beta_matrix = np.zeros((len(a_values),len(R0_values)))
+    q95_matrix = np.zeros((len(a_values),len(R0_values)))
+    B0_matrix = np.zeros((len(a_values),len(R0_values)))
+    BCS_matrix = np.zeros((len(a_values),len(R0_values)))
+    
+    # Initialisation
+    x = 0
+    y = 0
+    
+    # Scanning R0
+    for R0 in tqdm(R0_values, desc='Scanning parameters'):
+        
+        # Increment after iterations
+        y = 0
+        
+        # Scanning a
+        for a in a_values :
+            
+            # Main calcul
+            (B0_solution, B_CS, tauE_solution, Q_solution, Ip_solution, n_solution,
+            beta_solution, qstar_solution, q95_solution, nG_solution, 
+            P_CD, P_sep, P_Thresh, cost, heat, Gamma_n, f_alpha_solution, TF_ratio,
+            R0_a_solution, R0_a_b_solution, R0_a_b_c_solution, R0_a_b_c_d_solution) = calcul(a,R0, Bmax,P_fus)
+            
+            # Verifier les conditions
+            n_condition = n_solution / nG_solution
+            beta_condition = beta_solution / betaN
+            q_condition = q / qstar_solution
+            
+            max_limit = max(n_condition, beta_condition, q_condition)
+            
+            # Initialisation
+            radial_build = np.nan
+            max_limit_density = np.nan
+            max_limit_security = np.nan
+            max_limit_beta = np.nan
+            max_limit_power = np.nan
+            
+            if not np.isnan(R0_a_b_c_d_solution) and not np.isnan(max_limit) and max_limit<1 and R0_a_b_c_d_solution>0 :
+                radial_build = R0
+            
+            # Création d'un tableau contenant les valeurs des conditions
+            conditions = np.array([n_condition, beta_condition, q_condition])
+            # Indice de la condition la plus contraignante
+            indice_max_condition = np.argmax(conditions)
+            
+            if not np.isnan(max_limit) and max_limit < 2 :
+                # Action en fonction de la limite la plus contraignante
+                if indice_max_condition == 0:
+                    # Action spécifique pour n_condition
+                    max_limit_density = max_limit
+                elif indice_max_condition == 1:
+                    # Action spécifique pour beta_condition
+                    max_limit_beta = max_limit
+                elif indice_max_condition == 2:
+                    # Action spécifique pour q_condition
+                    max_limit_security = max_limit
+                    
+            # Store the value in the matrix
+            # Radial Build
+            radial_build_matrix[y,x] = radial_build       
+            # Plasma limits
+            max_limits_density[y, x] = max_limit_density
+            max_limits_security[y, x] = max_limit_security
+            max_limits_beta[y, x] = max_limit_beta
+            max_limits_matrix[y, x] = max_limit
+            # Details
+            Ip_matrix[y,x] = Ip_solution
+            n_matrix[y,x] = n_solution
+            beta_matrix[y,x] = beta_solution
+            q95_matrix[y,x] = q95_solution
+            B0_matrix[y,x] = B0_solution
+            BCS_matrix[y,x] = B_CS
+            # Figure of merits
+            Heat_matrix[y,x] = heat
+            Q_matrix[y,x] = Q_solution
+            P_CD_matrix[y,x] = P_CD
+            Gamma_n_matrix[y,x] = Gamma_n
+            Cost_matrix[y,x] = cost
+            L_H_matrix[y,x] = P_sep/P_Thresh
+            f_alpha_matrix[y,x] = f_alpha_solution*100
+            TF_ratio_matrix[y,x] = TF_ratio*100
+            
+            y = y+1
+            
+        x = x+1
+        
+    taille_police_topological_map = 15
+    taille_police_background_map = 15
+    taille_police_legende = 15
+    taille_police_other = 15
+    taille_titre = 22
+    plt.rcParams.update({'font.size': taille_police_other})
+
+    # Inverser l'ordre des lignes des matrices
+    inverted_matrix_density_limit = max_limits_density[::-1, :]
+    inverted_matrix_security_limit = max_limits_security[::-1, :]
+    inverted_matrix_beta_limit = max_limits_beta[::-1, :]
+    inverted_matrix_radial_build = radial_build_matrix[::-1, :]
+    inverted_matrix_plasma_limit = max_limits_matrix[::-1, :]
+
+    inverted_Ip_matrix = Ip_matrix[::-1, :]
+    inverted_n_matrix = n_matrix[::-1, :]
+    inverted_beta_matrix = beta_matrix[::-1, :]
+    inverted_q95_matrix = q95_matrix[::-1, :]
+    inverted_B0_matrix = B0_matrix[::-1, :]
+    inverted_BCS_matrix = BCS_matrix[::-1, :]
+    
+    inverted_Ip_matrix_mask = inverted_Ip_matrix
+    inverted_n_matrix_mask = inverted_n_matrix
+    inverted_beta_matrix_mask = inverted_beta_matrix
+    inverted_q95_matrix_mask = inverted_q95_matrix
+    inverted_B0_matrix_mask = inverted_B0_matrix
+    inverted_BCS_matrix_mask = inverted_BCS_matrix
+    # Créez un masque pour les valeurs NaN dans les matrices associées au Radial build
+    mask = np.isnan(inverted_matrix_radial_build)
+    # Appliquez ce masque à la nouvelle matrice
+    inverted_Ip_matrix_mask[mask] = np.nan
+    inverted_n_matrix_mask[mask] = np.nan
+    inverted_beta_matrix_mask[mask] = np.nan
+    inverted_q95_matrix_mask[mask] = np.nan
+    inverted_B0_matrix_mask[mask] = np.nan
+    inverted_BCS_matrix_mask[mask] = np.nan
+   
+    inverted_Heat_matrix = Heat_matrix[::-1, :]
+    inverted_Cost_matrix = Cost_matrix[::-1, :]
+    inverted_Q_matrix = Q_matrix[::-1, :]
+    inverted_P_CD_matrix = P_CD_matrix[::-1, :]
+    inverted_Gamma_n_matrix = Gamma_n_matrix[::-1, :]
+    inverted_L_H_matrix = L_H_matrix[::-1, :]
+    inverted_f_alpha_matrix = f_alpha_matrix[::-1, :]
+    inverted_TF_ratio_matrix = TF_ratio_matrix[::-1, :]
+
+    # Ask the user to choose the second topologic map :
+    chosen_isocontour = input("Choose the Iso parameter (Ip, n, beta, q95, B0, BCS): ")
+    chosen_topologic = input("Choose the background parameter (Heat, Cost, Q, Gamma_n, L_H, Alpha, TF): ")
+    
+    # Créer une figure et un axe principal
+    fig, ax = plt.subplots(figsize=(10, 12))
+    svg = f"Bmax={Bmax}_Pfus={P_fus}_scaling_law={law}_Elongation={κ}_Triangularity={δ}_Hfactor={H}_Meca={Choice_Buck_Wedg}_"
+    plt.title(f"$B_{{\mathrm{{max}}}}$ = {Bmax} [T], $P_{{\mathrm{{fus}}}}$ = {P_fus} [MW], scaling law : {law}",fontsize=taille_police_legende)
+    title_parameter = '$\mathbf{R_{0}}$'
+    plt.suptitle(f"Parameter space : a , {title_parameter}", fontsize=taille_titre,y=0.94, fontweight='bold')
+
+    # Calculer le minimum et le maximum des valeurs numériques
+    min_matrix = 0.5
+    max_matrix = 2
+    # Choix des couleurs
+    color_choice_density = 'Blues'
+    color_choice_security = 'Greens'
+    color_choice_beta = 'Reds'
+    # Afficher les heatmap pour les matrices avec imshow
+    im_density = ax.imshow(inverted_matrix_density_limit, cmap=color_choice_density, aspect='auto', interpolation='nearest', norm=Normalize(vmin=min_matrix, vmax=max_matrix))
+    im_security = ax.imshow(inverted_matrix_security_limit, cmap=color_choice_security, aspect='auto', interpolation='nearest', norm=Normalize(vmin=min_matrix, vmax=max_matrix))
+    im_beta = ax.imshow(inverted_matrix_beta_limit, cmap=color_choice_beta, aspect='auto', interpolation='nearest', norm=Normalize(vmin=min_matrix, vmax=max_matrix))
+
+    # Coutour limite plasma = 1
+    threshold = 1.0
+    ax.contour(inverted_matrix_plasma_limit, levels=[threshold], colors='#555555', linestyles='dashed')
+    grey_dashed_line = mlines.Line2D([], [], color='#555555', linestyle='dashed', label='Plasma stability boundary')
+
+    # Personnaliser les axes et le titre
+    plt.xlabel('$R_0$ [m]')
+    plt.ylabel('a [m]')
+
+    ### Color Bars
+
+    # Créer un axe supplémentaire en dessous de l'axe principal pour les colorbars
+    divider = make_axes_locatable(ax)
+    cax1 = divider.append_axes("bottom", size="5%", pad=0.9)
+    cax2 = divider.append_axes("bottom", size="5%", pad=0.1, sharex=cax1)
+    cax3 = divider.append_axes("bottom", size="5%", pad=0.1, sharex=cax1)
+
+    # Ajouter des annotations textuelles à côté des colorbars
+    cax1.annotate('n/$n_{\mathrm{G}}$', xy=(-0.01, 0.5), xycoords='axes fraction', ha='right', va='center', fontsize=taille_police_legende)
+    cax2.annotate(r'$\beta$/$\beta_{T}}$', xy=(-0.01, 0.5), xycoords='axes fraction', ha='right', va='center', fontsize=taille_police_legende)
+    cax3.annotate('$q_{\mathrm{K}}$/$q_{\mathrm{*}}$', xy=(-0.01, 0.5), xycoords='axes fraction', ha='right', va='center', fontsize=taille_police_legende)
+
+    # Créer les colorbars avec les orientations désirées
+    cbar_density = plt.colorbar(im_density, cax=cax1,orientation='horizontal')
+    tick_labels = cbar_density.ax.xaxis.get_ticklabels()
+    tick_labels[-1].set_visible(False)
+    cbar_beta = plt.colorbar(im_beta, cax=cax2, orientation='horizontal')
+    tick_labels = cbar_beta.ax.xaxis.get_ticklabels()
+    tick_labels[-1].set_visible(False)
+    cbar_security = plt.colorbar(im_security, cax=cax3, orientation='horizontal')
+    tick_labels = cbar_security.ax.xaxis.get_ticklabels()
+
+    ### Definition des axes
+
+    # Axe y (a_values)
+    y_indices = np.arange(0, len(a_values), 4)  # Sélectionner les indices des ticks
+    ax.set_yticks(y_indices)  # Positions
+    ax.set_yticklabels(np.round((a_max+a_min)-a_values[y_indices],2)) # Etiquettes
+
+    # Axe x (R0_values)
+    x_indices = np.arange(0, len(R0_values), 5)  # Sélectionner les indices
+    ax.set_xticks(x_indices)  # Positions
+    x_labels = [round(R0_values[i],2) for i in x_indices]  # Arrondir chaque valeur à une decimale
+    ax.set_xticklabels(x_labels, rotation=45, ha='right')  # Etiquettes
+
+    ### Ajouter des contours
+
+    # Remplacer NaN par -1 et les autres par 1 dans la matrice pour les contours du radial build
+    filled_matrix = np.where(np.isnan(inverted_matrix_radial_build), -1, 1)
+    # Définir le niveau de contour pour les valeurs de transition
+    contour_level = [0]
+    # Tracer les contours autour des valeurs de transition
+    contour_radial_line = ax.contour(
+    filled_matrix, 
+    levels=contour_level,
+    colors='black'   # Noir
+    )
+    # Création d'une ligne de légende personnalisée correspondant au style du contour
+    black_line = mlines.Line2D([], [], color='black', label='Radial build limit')
+    
+    # Tracer les isocontours choisis précédemment
+    if chosen_isocontour == 'Ip':
+        contour_lines = ax.contour(inverted_Ip_matrix_mask, levels=np.arange(1, 25,1), colors='#555555')
+        ax.clabel(contour_lines, inline=True, fmt='%d', fontsize=taille_police_topological_map)
+        grey_line = mlines.Line2D([], [], color='#555555', label='$I_p$ [MA]')
+    elif chosen_isocontour == 'n':
+        contour_lines = ax.contour(inverted_n_matrix_mask, levels=np.arange(0.25, 5,0.25), colors='#555555')
+        ax.clabel(contour_lines, inline=True, fmt='%.2f', fontsize=taille_police_topological_map)
+        grey_line = mlines.Line2D([], [], color='#555555', label='$n$ [10e20]')
+    elif chosen_isocontour == 'beta':
+        contour_lines = ax.contour(inverted_beta_matrix_mask, levels=np.arange(1, 10,0.25), colors='#555555')
+        ax.clabel(contour_lines, inline=True, fmt='%.2f', fontsize=taille_police_topological_map)
+        grey_line = mlines.Line2D([], [], color='#555555', label='$\\beta$ [%]')
+    elif chosen_isocontour == 'q95':
+        contour_lines = ax.contour(inverted_q95_matrix_mask, levels=np.arange(1, 10,0.5), colors='#555555')
+        ax.clabel(contour_lines, inline=True, fmt='%.1f', fontsize=taille_police_topological_map)
+        grey_line = mlines.Line2D([], [], color='#555555', label='$q_{95}$ []')
+    elif chosen_isocontour == 'B0':
+        contour_lines = ax.contour(inverted_B0_matrix_mask, levels=np.arange(1, 25 ,0.5), colors='#555555')
+        ax.clabel(contour_lines, inline=True, fmt='%.1f', fontsize=taille_police_topological_map)
+        grey_line = mlines.Line2D([], [], color='#555555', label='$B_{0}$ [T]')
+    elif chosen_isocontour == 'BCS':
+        contour_lines = ax.contour(inverted_BCS_matrix_mask, levels=np.arange(1, 100 ,2), colors='#555555')
+        ax.clabel(contour_lines, inline=True, fmt='%.1f', fontsize=taille_police_topological_map)
+        grey_line = mlines.Line2D([], [], color='#555555', label='$B_{CS}$ [T]')
+    else:
+        print('Choose a relevant Iso parameter')
+
+    if chosen_topologic == 'Heat':
+        # Heat contours
+        contour_lines_Heat = ax.contour(inverted_Heat_matrix, levels=np.arange(1000, 10000 ,500), colors='white')
+        ax.clabel(contour_lines_Heat, inline=True, fmt='%d', fontsize=taille_police_background_map)
+        white_line = mlines.Line2D([], [], color='white', label='Heat// [MW-T/m]')
+    elif chosen_topologic == 'Q':
+        # Q contours
+        contour_lines_Q = ax.contour(inverted_Q_matrix, levels=np.arange(0, 60 ,10), colors='white')
+        ax.clabel(contour_lines_Q, inline=True, fmt='%d', fontsize=taille_police_background_map)
+        white_line = mlines.Line2D([], [], color='white', label='Q []')
+    elif chosen_topologic == 'Cost' :
+        # Cost contours
+        contour_lines_cost = ax.contour(inverted_Cost_matrix, levels=np.arange(0, 1000,20), colors='white')
+        ax.clabel(contour_lines_cost, inline=True, fmt='%d', fontsize=taille_police_background_map)
+        white_line = mlines.Line2D([], [], color='white', label='$Cost$ [$m^3$]')
+    elif chosen_topologic == 'Gamma_n' :
+        # Gamma_n contours
+        contour_lines_Gamma_n = ax.contour(inverted_Gamma_n_matrix, levels=np.arange(-1, 20,1), colors='white')
+        ax.clabel(contour_lines_Gamma_n, inline=True, fmt='%d', fontsize=taille_police_background_map)
+        white_line = mlines.Line2D([], [], color='white', label='$\\Gamma_n$ [MW/m²]')
+    elif chosen_topologic == 'L_H' :
+        # L-H transition contours
+        contour_lines_LH = ax.contour(inverted_L_H_matrix, levels=np.arange(-1, 10,1), colors='white')
+        ax.clabel(contour_lines_LH, inline=True, fmt='%d', fontsize=taille_police_background_map)
+        white_line = mlines.Line2D([], [], color='white', label='L-H Transition []')
+    elif chosen_topologic == 'Alpha' :
+        # Alpha fraction contours
+        contour_lines_Alpha = ax.contour(inverted_f_alpha_matrix, levels=np.arange(-1, 100,1), colors='white')
+        ax.clabel(contour_lines_Alpha, inline=True, fmt='%d', fontsize=taille_police_background_map)
+        white_line = mlines.Line2D([], [], color='white', label='Alpha fraction [%]')
+    elif chosen_topologic == 'TF' :
+        # TF ratio contours
+        contour_lines_TF = ax.contour(inverted_TF_ratio_matrix, levels=np.arange(0, 100,5), colors='white')
+        ax.clabel(contour_lines_TF, inline=True, fmt='%d', fontsize=taille_police_background_map)
+        white_line = mlines.Line2D([], [], color='white', label='TF centering fraction [%]')
+    else :
+        print('Choose a relevant background parameter')
+    # Légende
+    ax.legend(handles=[grey_line,white_line, grey_dashed_line,black_line], loc='upper left', facecolor='lightgrey', fontsize=taille_police_legende)
+
+    # Save the image
+    # Remplacer les virgules par des points dans svg
+    svg = svg.replace('.', ',')
+    path_to_save = os.path.join(save_directory,f"a_and_R0_scan_with_{svg}.png")
+    plt.savefig(path_to_save, dpi=300, bbox_inches='tight')
+    # Afficher la figure
+    plt.show()
+
+    # Reinitialisation des paramètres par defaut
+    plt.rcdefaults()
+        
+if __name__ == "__main__":
+    Bmax = 12
+    P_fus = 2000
+    R0_a_scan(Bmax,P_fus,κ,δ)
+        
+        
