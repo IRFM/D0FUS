3b7400a2e010c491eaf1a68cff98aff4a360fd06
AUCLAIR Timothé 276941 <timothe.auclair@cea.fr>
2025-01-09 13:52:00 +0000

    Upload New File

---
+++ D0FUS_BIB/D0FUS_initialisation.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 25 13:30:05 2024
+
+@author: TA276941
+"""
+
+from D0FUS_plot import *
+
+# Ajouter le répertoire 'D0FUS_BIB' au chemin de recherche de Python
+sys.path.append(os.path.join(os.path.dirname(__file__), 'D0FUS_BIB'))
+
+
+#%%
+
+print("D0FUS_init loaded")
