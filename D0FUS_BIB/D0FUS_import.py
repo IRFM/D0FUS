82530440a26872776f550aa74eb6f31e8b2db90f
AUCLAIR Timothé 276941 <timothe.auclair@cea.fr>
2025-01-09 13:51:42 +0000

    Upload New File

---
+++ D0FUS_BIB/D0FUS_import.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 25 13:30:05 2024
+
+@author: TA276941
+"""
+
+#%% Import
+
+# Importations de bibliothèques standards
+import os
+import math
+import time
+import warnings
+import sys
+import importlib
+
+# Importations de bibliothèques scientifiques et de calcul numérique
+import numpy as np
+import pandas as pd
+import sympy as sp
+from scipy.optimize import (
+    fsolve,
+    differential_evolution,
+    root_scalar,
+    minimize_scalar,
+    minimize,
+    bisect
+)
+from scipy.interpolate import griddata
+from scipy.integrate import quad
+
+# Importations de bibliothèques de visualisation
+import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
+import matplotlib.colors as mcolors
+import matplotlib.patches as mpatches
+from matplotlib.colors import Normalize
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from mpl_toolkits.mplot3d import Axes3D
+import seaborn as sns  # améliore l'esthétique des graphiques
+from pandas.plotting import table
+from tqdm import tqdm
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+# Importation pour le traitement des images
+import imageio
+
+#%%
+
+print("D0FUS_import loaded")
+
