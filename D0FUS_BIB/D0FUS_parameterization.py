7a71f73381d205d0d7cab670a5829d9f34d72f54
AUCLAIR Timothé 276941 <timothe.auclair@cea.fr>
2025-06-20 06:49:14 +0000

    Update D0FUS_parameterization.py

--- D0FUS_BIB/D0FUS_parameterization.py
+++ D0FUS_BIB/D0FUS_parameterization.py
@@ -14,24 +14,25 @@ sys.path.append(os.path.join(os.path.dirname(__file__), 'D0FUS_BIB'))
 #%% Panel control
 
 # Parameterization
-Supra_choice = 'HTS'                # 'Manual' , 'HTS' or 'LTS'
-Chosen_Steel = '316L'               # '316L' , 'NH50' or 'Manual'
-Radial_build_model = 'D0FUS'        # "academic" , "D0FUS" , "CIRCEE"
+Supra_choice = 'HTS'                # 'HTS' , 'LTS' or Manual'
+Chosen_Steel = 'NH50'               # '316L' , 'NH50' or 'Manual'
+Radial_build_model = 'academic'        # "academic" , "D0FUS" , "CIRCEE"
 Choice_Buck_Wedg = 'Wedging'        # 'Wedging' or 'Bucking'
 Option_Kappa = 'Wenninger'          # 'Stambaugh' , 'Freidberg' , 'Wenninger' or 'Manual'
 L_H_Scaling_choice = 'New_Ip'       # 'Martin' , 'New_S', 'New_Ip'
 Scaling_Law = 'IPB98(y,2)'          # 'IPB98(y,2)', 'ITPA20-IL', 'ITPA20', 'DS03', 'L-mode', 'L-mode OK', 'ITER89-P'
+Bootstrap_choice = 'Freidberg'      # 'Freidberg' or 'Segal'
 
 # Inputs
 H = 1                               # H factor
 Tbar  = 14                          # Mean Temperature keV
-Temps_Plateau = 0                   # Plateau time
-δ = 0.5                             # Triangularity
 b = 1.2                             # BB + 1rst Wall + N shields + Gaps
+Temps_Plateau = 0                   # Plateau time
 
 #%% Constants initialisation
 
 Choice_solving_CS_method = "brentq" # "brentq" or "manual" for debuging
+Choice_solving_TF_method = "brentq" # "brentq" or "manual" for debuging
 
 # Physical constants
 E_ELEM  = 1.6e-19           # Electron charge [Coulomb]
@@ -45,6 +46,7 @@ E_ALPHA = 3.5 *1.E6*E_ELEM  # Joule
 E_N     = 14.1*1.E6*E_ELEM  # Joule
 E_F     = 22.4*1.E6*E_ELEM  # Joule (considering all N react with Li)
 Atomic_mass  = 2.5          # average atomic mass in AMU
+Zeff = 1                    # Zeff du plasma
 
 # Plasma stability
 betaN = 2.8  # Beta Tryon limit
@@ -57,17 +59,15 @@ C_Alpha = 5     # Helium dilution tuning parameter
 
 # Flux
 Ce = 0.45            # Ejima constants
-Li = 0.85            # Internal inductance of the plasma -> To be replaced with proper model
 ITERPI = 20          # ITER plasma induction current [Wb]
 
 # TF
-gamma_TF = 1/2       # fraction d'acier pouvant soutenir les efforts suivant l'axe considéré (r ou theta) = facteur de concentration de contrainte dépendant de la géométrie du CICC
-coef_inboard_tension = 1/2 # Facteur de répartition de la tension entre jambe interne et externe
+coef_inboard_tension = 1/2 # Paramètre représentatn la répartition jambe interne / externe de la tension
 F_CClamp = 0e6       # C-Clamp limit in N , max order of magnitude from DDD : 30e6 N and of 60e6 N from [Bachmann (2023) FED]
+n_TF = 1/2           # Asymetry conductor parameter 
 
 # CS
 Gap = 0.1            # Gap between wedging and bucking CS and TF [m]
-gamma_CS = 1/2       # fraction d'acier pouvant soutenir les efforts suivant l'axe considéré (r ou theta) = facteur de concentration de contrainte dépendant de la géométrie du CICC
 
 # Efficienty      
 eta_T = 0.4          # Ratio between thermal and electrical power
@@ -79,9 +79,11 @@ theta_deg = 2.7      # Angle d'incidence sur les PFU pour calcul du flux de chal
 # Source 2: “SOLPS-ITER simulations of the ITER divertor with improved plasma conditions,” Journal of Nuclear Materials (2024) )
 
 # Current density scaling
-T_helium = 4.2 #○ K temeprature de l'helium considéré
-Marge_T_Helium = 0.3 # Puisque l'helium est à 10 barre, il est poussé à 0.3 K au dessus de la consigne
-
+T_helium = 4.2           # K temeprature de l'helium considéré
+Marge_T_Helium = 0.3     # Puisque l'helium est à 10 barre, il est poussé à 0.3 K au dessus de la consigne
+f_Cu = 0.5               # Copper fraction
+f_Cool = 0.7             # Cooling fraction
+f_In = 0.75              # Insulation fraction
 
 #%% Numerical initialisation
 
