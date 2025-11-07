09a9dd45411576b482943d9115de24d40fdd9fd2
Auclair Timothe <timothe.auclair@cea.fr>
2025-11-07 10:42:32 +0100

    Create D0FUS.py main entry point (placeholder)

---
+++ D0FUS.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+D0FUS - Fusion Reactor Design Tool
+Main entry point (to be updated in Phase 3)
+
+Author: Timothe Auclair
+"""
+
+# TODO: Add interactive mode and CLI in Phase 3
+print("D0FUS main entry point - under construction")
